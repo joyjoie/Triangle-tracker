5f808fa6d8fd386f6fbbdc6c2b2972dc4714e1ee
joy <wendokingoo@gmail.com>
2018-12-03 07:55:26 +0300

    javascript variables declared

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+function process() {
+
+var sideA = parseInt(document.getElementById("a").value);
+var sideB = parseInt(document.getElementById("b").value);
+var sideC = parseInt(document.getElementById("c").value);
+}
