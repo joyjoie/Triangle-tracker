4890864488b3b8c6e8d1e689c3ab09fdfb8bfcc8
joy <wendokingoo@gmail.com>
2018-12-03 07:57:00 +0300

    if else statements commited

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,19 @@ function process() {
 var sideA = parseInt(document.getElementById("a").value);
 var sideB = parseInt(document.getElementById("b").value);
 var sideC = parseInt(document.getElementById("c").value);
+
+if(sideA === sideB && sideB === sideC && sideA ===sideC){
+document.getElementById("result").value = "Equilateral"
+}
+else if (sideA ===sideB || sideB === sideC ||sideC === sideA){
+  document.getElementById("result").value ="Isosceles"
+}
+else if (sideA!= sideB!=sideC){
+    if (((sideA + sideB)<= sideC) ||((sideB+sideC)<= sideA) ||((sideA+sideC)<=sideB)) {
+document.getElementById("result").value ="Not a triangle"
+}
+else {
+  document.getElementById("result").value="Scalene"
+}
+}
 }
