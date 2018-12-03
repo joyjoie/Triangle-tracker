e995fe13da444effbdf59a4bf78fd69f3f45954b
joy <wendokingoo@gmail.com>
2018-12-03 09:03:00 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,17 @@
+#Triangle tracker
+#### By Joy King'oo
+## Description
+this project allows one to input numeric values and allows one to do calculations to determine what type of triangle it is.
+## Setup/Installation Requirements
+* Must have a modern browser
+* Must have a text editor
+* Must have a github account
+* Must fork clone project to my repository
+## Known Bugs
+No known bugs
+## Technologies Used
+HTML Javascript and CSS have been used for development
+## Support and contact details
+if you have any concerns or issues regarding my current work reach out through my github account
+### MIT License
+Copyright (c) 2018 **Joywendo**
--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,30 @@
 function process() {
-var Z=["","",""]
+var Z= ["","",""];
 Z[0] = parseInt(document.getElementById("a").value);
 Z[1]= parseInt(document.getElementById("b").value);
 Z[2] = parseInt(document.getElementById("c").value);
 
-if(Z[0]) === Z[1] && Z[1]=== Z[2] && Z[2] ===Z[0]{
+if (Z[0] === Z[1] && Z[1]===Z[2]) {
 document.getElementById("result").value = "Equilateral"
 }
-else if (Z[0] ===Z[1] || Z[1] === Z[2]||Z[2] ===Z[0]{
+
+
+else if (Z[0] ===Z[1] || Z[1] === Z[2]||Z[2] ===Z[0]){
   document.getElementById("result").value ="Isosceles"
 }
 else if (Z[0]!= Z[1]!=Z[2]){
-    if (((Z[0] +Z[1])<= Z[2]) ||((Z[1]+Z[2]<= Z[0]) ||((Z[0]+Z[2])<=Z[1])) {
+    if (((Z[0] +Z[1])<= Z[2]) ||((Z[1]+Z[2]<= Z[0]) ||((Z[0]+Z[2])<=Z[1]))) {
 document.getElementById("result").value ="Not a triangle"
 }
 else {
   document.getElementById("result").value="Scalene"
 }
 }
+}
 function resetForm(){
 
-  document.getElementById("button2").value ="";
+  document.getElementById("a").value ="";
+  document.getElementById("b").value ="";
+  document.getElementById("a").value ="";
+  document.getElementById("result").value ="";
 }
